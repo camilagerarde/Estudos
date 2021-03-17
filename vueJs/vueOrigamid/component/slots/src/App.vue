919111733690282d5c7d6a01b8feaab016f90764
Camila Cunha <61171512+camilagerarde@users.noreply.github.com>
2021-03-17 12:13:50 -0300

    Add files via upload

---
+++ vueJs/vueOrigamid/component/slots/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <modal-alerta>
+			<template v-slot:header>
+				<h1>Header do Slot</h1>
+			</template>
+      <template v-slot:default="{ user }">
+        {{user.name}}
+        <p>{{mensagem}}</p>
+      </template>
+			<template v-slot:footer>
+				<p>Esse é o footer do slot.</p>
+			</template>
+		</modal-alerta>
+  </div>
+</template>
+
+<script>
+import ModalAlerta from "./components/ModalAlerta.vue";
+
+  export default {
+    name: "app",
+    components: {
+      ModalAlerta
+    },
+    data() {
+      return {
+        mensagem: "Isso vai aparecer no slot."
+      };
+    }
+  };
+</script>
+
+<style>
+
+</style>
---
+++ vueJs/vueOrigamid/component/slots/src/components/ModalAlerta.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modal">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <section>
+      <slot :user="user"></slot>
+      <button>Comprar</button>
+    </section>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ModalAlerta",
+    data() {
+      return {
+        user: {
+          name: 'Nome do usuário'
+        }
+      }
+    }
+  };
+</script>
+<style>
+  header {
+    border-bottom: 2px solid;
+  }
+  section {
+    margin: 20px;
+  }
+  footer {
+    border-top: 2px solid;
+  }
+</style>
