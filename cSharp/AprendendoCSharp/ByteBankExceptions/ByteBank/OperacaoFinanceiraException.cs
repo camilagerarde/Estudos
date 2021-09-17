0ec285346ea883e202eaadb6eafd45385e5188fe
Camila Cunha <61171512+camilagerarde@users.noreply.github.com>
2021-09-17 14:28:00 -0300

    Add files via upload

---
+++ cSharp/AprendendoCSharp/ByteBankExceptions/ByteBank/OperacaoFinanceiraException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException()
+        {
+        
+        }
+        public OperacaoFinanceiraException(string mensagem) 
+            : base(mensagem)
+        {
+
+        }
+        public OperacaoFinanceiraException(String mensagem, Exception excecaoInterna)
+            :base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
---
+++ cSharp/AprendendoCSharp/ByteBankExceptions/ByteBank/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(6540,564640);
+                ContaCorrente contaDois = new ContaCorrente(6540, 564644);
+                conta.Transferir(50000, contaDois);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ParamName);
+            }
+            catch (SaldoInsuficienteException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                Console.WriteLine("INNER EXCEPTION");
+                Console.WriteLine(e.InnerException.Message);
+                Console.WriteLine(e.InnerException.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Ocorreu um erro com a divisão de {numero} por {divisor}");
+                throw;
+            }
+        }
+    }
+}
